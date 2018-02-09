bb29cff4ca6022b7ba5019cc2f64ddc2de1a28bd
DANIELLE NAOMI NAKATSU <38386161892@jpmorgan.sp>
2018-02-09 21:54:03 -0200

    sidebar done css changes

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementsByClassName("nav-menu-button").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementsByClassName("nav-menu-button-close").style.width = "0";
+}
